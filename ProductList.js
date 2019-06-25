3bcf379b693087393c51707507b7dd363b474742
Dewangan <dneelam@amazon.com>
2019-06-25 17:32:27 +0530

    First Commit

---
+++ ProductList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Product from Product;
+
+class app extends React.component{
+    render(){
+        return {    
+            <div class="product-conainer"></div>       
+            
+        }
+    }
+}
+
+export default ProductList;
---
+++ app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductList from ProductList;
+
+class app extends React.component{
+    render(){
+        return {           
+            
+        }
+    }
+}
